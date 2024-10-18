afc6cfc1dec09a66cfadd45b0d3e5a3692d7bf72
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-10-18 14:39:20 -0300

    configurando puerto

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import express from 'express'
-console.log('Hola Mundo')
 
+//1- configurar puerto
+const app = express ();
+
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), ()=>{
+    console.info('Estoy escuchando el puerto '+ app.get('port'))
+
+})
+
+
+//2- configurar middlewares
+
+//3- configurar rutas
